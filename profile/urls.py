4fb648e44a1935c5e169b22d1f59bf257c70fe9a
Rafael Park <parkrafael11@gmail.com>
2023-09-13 22:34:09 -0700

    styling done for frontpage, login, register

--- profile/urls.py
+++ profile/urls.py
@@ -6,10 +6,7 @@ from . import views
 app_name = "profile"
 
 urlpatterns = [
-    # root page
     path('', views.index, name = 'index'),
-    # register page
     path('register/', views.register_request, name = 'register'),
-    # login page
     path('login/', views.login_request, name = 'login'),
 ]
